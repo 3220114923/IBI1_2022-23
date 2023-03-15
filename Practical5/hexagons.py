372ec50b1f7c90543210eda2668e4883bedf6d7c
Jonas <linfeng.22@intl.zju.edu.cn>
2023-03-15 20:43:33 +0800

    edit hexagons

---
+++ Practical5/hexagons.py
@@ -0,0 +1,12 @@
+# Initialize the n to 1
+n = 1
+# Initialize the h to 1
+h = 1
+# limit n to range(1,5)
+while n <=5:
+# Import h function ro n
+      h = 2*n*(2*n-1)/2
+# Increment the n count by 1
+      n += 1
+# print the result
+      print(h)
