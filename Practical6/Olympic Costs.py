6cab3bfb43b3e7f5743a489e320165ab60b26482
Jonas <linfeng.22@intl.zju.edu.cn>
2023-03-22 16:23:31 +0800

    edit Olympic Costs.

--- Practical6/Olympic Costs.py
+++ Practical6/Olympic Costs.py
@@ -10,5 +10,6 @@ plt.ylabel('Costs')
 plt.title('Olympic Costs')
 plt.xticks(ind, ('LA 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012'))
 #plt.yticks(ind, (0, 81, 10))
+plt.xticks(fontsize=6)
 
 plt.show()
